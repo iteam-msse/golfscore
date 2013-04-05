b5128a484433259b354a2afe658e3d2eefc9bd05
Kevin Peterson <kevin.peterson@mayo.edu>
2013-04-04 22:27:35 -0500

    Added some test cases.

--- test.py
+++ test.py
@@ -13,9 +13,34 @@ def test(test_number, cmd, logfile=test_log):
 	ret_code = p.wait()
 	logfile.flush()
 	logfile.write("\nResult Code: " + str(ret_code) + "\n")
+	logfile.write("\nOutput File(s):\n")
+	logfile.flush()
+	for filename in os.listdir("out"):
+		logfile.write(" * " + filename + "\n")
+		logfile.flush()
+		os.remove("out/"+filename)
 	logfile.write("==================================================\n\n")
 	logfile.flush()
 
 
 test(1, './golfscore')
-test(2, './golfscore -h')
+test(2, './golfscore -asdf')
+test(5, './golfscore -hctg testdata/in.txt out')
+test(6, './golfscore -h testdata/in.txt out')
+test(7, './golfscore -c testdata/in.txt out')
+test(8, './golfscore -t testdata/in.txt out')
+test(9, './golfscore -g testdata/in.txt out')
+test(10, './golfscore -ctg testdata/in.txt out')
+test(12, './golfscore -c out')
+test(13, './golfscore -c testdata/__INVALID__.txt out')
+test(14, './golfscore -h')
+test(15, './golfscore -hctg testdata/in.txt out')
+test(16, './golfscore -ctg testdata/invalid_in.txt out')
+test(17, './golfscore -ctg testdata/invalid_in.txt out/__INVALID_DIR__')
+test(18, './golfscore testdata/invalid_in.txt -ctg out/__INVALID_DIR__')
+test(19, './golfscore testdata/invalid_in.txt out/__INVALID_DIR__ -ctg')
+test(20, './golfscore -ctg testdata/in.txt out extra parameters here')
+
+
+
+
